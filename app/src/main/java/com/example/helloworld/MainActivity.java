b536a2ac9e09c2d6821119907397678426199151
SP6 <qwer14788lll@163.com>
2020-05-25 14:57:09 +0800

    登录功能：增加默认账号密码判断

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,8 +24,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //intent：意图分为显示和隐式
                 //new Intent的参数上下文，目标activity的类
-                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(intent);
+                //默认手机号码10086，默认密码123456
+                String phone=mBinding.editPhone.getText().toString();
+                String pwd=mBinding.editPwd.getText().toString();
+                if(phone.equals("10086")&&pwd.equals("123456"))
+                {
+                    Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this,"手机号或密码错误",Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
