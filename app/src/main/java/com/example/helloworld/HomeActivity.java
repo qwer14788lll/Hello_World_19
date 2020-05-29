27cdb306f5462461dfdbf137e87657f10afe9025
SP6 <qwer14788lll@163.com>
2020-05-29 10:59:33 +0800

    实现功能：将用户资料显示到Home界面

--- app/src/main/java/com/example/helloworld/HomeActivity.java
+++ app/src/main/java/com/example/helloworld/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.helloworld.databinding.ActivityHomeBinding;
@@ -18,6 +19,15 @@ public class HomeActivity extends AppCompatActivity {
 
         Intent intent=getIntent();//获取传过来的Intent对象
         String phone=intent.getStringExtra("data_phone");//取出对应的值
-        mBinding.textPhone.setText(phone);
+        mBinding.userPhone.setText(phone);
+
+        SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
+        String userName= sp.getString("name_" + phone,"0");
+        String userSex=sp.getString("sex_" + phone,"0");
+        String userSms= sp.getString("sms_" + phone, "0").equals("1") ?"接受":"不接受";
+        mBinding.userName.setText(userName);
+        mBinding.userSex.setText(userSex);
+        String temp=mBinding.userSms.getText().toString()+"："+userSms;//接受消息推送：接受/不接受
+        mBinding.userSms.setText(temp);
     }
 }
