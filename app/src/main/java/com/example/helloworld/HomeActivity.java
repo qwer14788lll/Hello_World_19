3e31962f3a41361365df863ee5023ac46014eaa0
SP6 <qwer14788lll@163.com>
2020-06-05 10:08:54 +0800

    HomeActivity实现功能：快速双击退出键，退出登录

--- app/src/main/java/com/example/helloworld/HomeActivity.java
+++ app/src/main/java/com/example/helloworld/HomeActivity.java
@@ -5,29 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.helloworld.databinding.ActivityHomeBinding;
 
+/**
+ * @author Surface Pro 6
+ */
 public class HomeActivity extends AppCompatActivity {
 
     private ActivityHomeBinding mBinding;
+    private long exitTime = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding=ActivityHomeBinding.inflate(getLayoutInflater());
+        mBinding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
-
-        Intent intent=getIntent();//获取传过来的Intent对象
-        String phone=intent.getStringExtra("data_phone");//取出对应的值
+        //获取传过来的Intent对象
+        Intent intent = getIntent();
+        //取出对应的值
+        String phone = intent.getStringExtra("data_phone");
         mBinding.userPhone.setText(phone);
 
         SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
-        String userName= sp.getString("name_" + phone,"0");
-        String userSex=sp.getString("sex_" + phone,"0");
-        String userSms= sp.getString("sms_" + phone, "0").equals("1") ?"接受":"不接受";
+        String userName = sp.getString("name_" + phone, "0");
+        String userSex = sp.getString("sex_" + phone, "0");
+        String userSms = "1".equals(sp.getString("sms_" + phone, "0")) ? "接受" : "不接受";
         mBinding.userName.setText(userName);
         mBinding.userSex.setText(userSex);
-        String temp=mBinding.userSms.getText().toString()+"："+userSms;//接受消息推送：接受/不接受
+        //接受消息推送：接受/不接受
+        String temp = mBinding.userSms.getText().toString() + "：" + userSms;
         mBinding.userSms.setText(temp);
+    }
+
+    /**
+     * 拦截系统返回键
+     */
+    @Override
+    public void onBackPressed() {
+        exit();
+    }
+
+    private void exit() {
+        long time = 2000;
+        if (System.currentTimeMillis() - exitTime > time) {
+            //存储此次点击返回键的时间
+            exitTime = System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(), "快速点击两次，退出当前账号", Toast.LENGTH_SHORT).show();
+        } else {
+            finish();
+        }
     }
 }
