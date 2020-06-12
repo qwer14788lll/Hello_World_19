3bbb76ffd1482619af0643006965bebb5c30aa05
SP6 <qwer14788lll@163.com>
2020-06-12 12:05:44 +0800

    RegisterActivity.java：规范括号

--- app/src/main/java/com/example/helloworld/RegisterActivity.java
+++ app/src/main/java/com/example/helloworld/RegisterActivity.java
@@ -76,7 +76,9 @@ public class RegisterActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == R.id.radio_man) {
                     sex = mBinding.radioMan.getText().toString();
-                } else sex = mBinding.radioWomen.getText().toString();
+                } else {
+                    sex = mBinding.radioWomen.getText().toString();
+                }
             }
         });
     }
